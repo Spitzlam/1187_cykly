b7b7ada6a5308547597a5b3264c489e15738fef3
Michal Sifalda <spitzlam09@gmail.com>
2023-11-30 09:25:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+int userinput1 = 17;
+for (int i = userinput1;i <= 20; i++ )
+{
+    Console.WriteLine(i);
+}
+//
+//
+bool proceed = true;
+while (proceed)
+{
+    string userinput2 = Console.ReadLine();
+    if (userinput2 == "pls help")
+    {
+        proceed = false;
+    }
+    else
+    {
+        for (int i = 1;  i <= 3; i++ )
+        {
+            Console.WriteLine(userinput2);
+        }
+    }
+}
+*/
+//
+Random generator = new Random();
+bool alpha = true;
+while (alpha)
+{
+    string input = Console.ReadLine();
+    if (input != "ne")
+    {
+        Console.WriteLine(generator.Next(1, 10));
+    }
+    else
+    {
+        alpha = false;
+    }
+}
+//
+//
+for (char i = 'R'; i <= 'Z'; i++)
+{
+    Console.WriteLine(i);
+}
+
